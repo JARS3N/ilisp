511151ea12c76db5f4b8e357c64814ba634cc437
JARS3N <JARS3N@users.noreply.github.com>
2020-05-19 16:05:24 -0400

    Create stats.lisp

---
+++ stats.lisp
@@ -0,0 +1,21 @@
+;;; some simple stats to calculate
+  
+(defun sum (x)(reduce #'+ x)) 
+(defun mean (x)(* (/ (sum x) (length x)) 1.0)
+(defun mean-diff (x)(mapcar #'(lambda (u) (-  (mean x) u)) x))
+(defun ^2 (x) (expt x 2))
+(defun var (x) (/ (sum (mapcar #'^2 (mean-diff2 x))) (- (length x) 1.0)))
+(defun sd (x) (expt (var x) .5))
+(defun absdev ())
+(defun cv (x) (* 100 (/ (mean x) (sd x))))
+(defun ssmd (X Y)
+  (/
+    (- (mean X) (mean Y))
+    (expt (+ (sd X) (sd Y)) .5)
+      )
+  )
+
+(defun z-factor (X Y)
+ (- 1 (/ (* 3 (+ (sd X) (sd Y)))
+    (abs (- (mean X) (mean Y))))
+  ))
