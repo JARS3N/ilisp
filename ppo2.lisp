a441768c742219e35ddcb25a68cb2d2296569e9c
JARS3N <JARS3N@users.noreply.github.com>
2020-08-29 12:20:25 -0400

    relaized the lexical scoping issue,replaced setf with let

--- ppo2.lisp
+++ ppo2.lisp
@@ -3,11 +3,11 @@
 ;; atm is atmospheric pressure in mmHg
 ;; conditonals are due to phase changes @30C
   "Calculate Partial Pressure of O2 in liquid"
-  (if (null atm) (setf atm 760))
-  (setf coef (if (>= tc 30)  0.827 0.678))
-  (setf adj (if (>= tc 30) 49 35))
-  (setf exps (mapcar #'expt (make-list 4 :initial-element TC) '(3 2 1 0)))
-  (* 0.005625 atm coef (/ 1 (+ adj TC))   
-    (reduce #'+ (mapcar #'* '(-0.0000058333 0.0001821 0.072405 2.5443) exps))
-                 (- atm (reduce '+ (mapcar #'* '(0.0456 -0.8559 16.509) (cdr exps)))))  
-  )
+   (let (
+	 (atm (if (null atm) 760 atm))
+	 (coef (if (>= tc 30d0)  0.827d0 0.678d0))
+	 (adj (if (>= tc 30d0) 49d0 35d0))
+	 (exps (mapcar #'expt (make-list 4 :initial-element TC) '(3 2 1 0))))
+       (* 0.005625d0 atm coef (/ 1 (+ adj TC))   
+    (reduce #'+ (mapcar #'* '(-0.0000058333d0 0.0001821d0 0.072405d0 2.5443d0) exps))
+                 (- atm (reduce '+ (mapcar #'* '(0.0456d0 -0.8559d0 16.509d0) (cdr exps)))))))
